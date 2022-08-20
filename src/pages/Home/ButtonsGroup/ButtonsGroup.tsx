95caf300a3b3e2a2d6cd20ce9262ce60dc35d89c
Roy Canani <roy33441c@gmail.com>
2022-08-20 19:48:45 +0300

    finish

--- src/pages/Home/ButtonsGroup/ButtonsGroup.tsx
+++ src/pages/Home/ButtonsGroup/ButtonsGroup.tsx
@@ -1,45 +1,46 @@
-import { FC } from 'react';
-import useTheme from '@material-ui/core/styles/useTheme';
+import { FC } from "react";
+import useTheme from "@material-ui/core/styles/useTheme";
 
-import LinkButton from 'components/LinkButton/LinkButton';
-import { useStyles } from './ButtonsGroupStyles';
+import LinkButton from "components/LinkButton/LinkButton";
+import { useStyles } from "./ButtonsGroupStyles";
 
 const ButtonsGroup: FC = (): JSX.Element => {
   const theme = useTheme();
   const classes = useStyles();
 
   const sitesURLs = {
-    report: 'https://one.prat.idf.il/',
-    idf: 'https://www.idf.il/',
-    moodle: 'https://campus.digital.idf.il/',
-    drive: 'https://drive.google.com/drive/folders/1GOJ1MUcvZ6DGpi_-2Hu5g8XthiKdmSYR'
+    report: "https://one.prat.idf.il/",
+    idf: "https://www.idf.il/",
+    moodle: "https://campus.digital.idf.il/",
+    drive:
+      "https://drive.google.com/drive/folders/1F9Npaoi4llEOJcX92dnrHpBz1bHwDzdn?usp=sharing",
   };
 
   return (
     <div className={classes.container}>
       <LinkButton
         siteURL={sitesURLs.report}
-        iconURL='icons/report.svg'
+        iconURL="icons/report.svg"
         label='דו"ח 1'
         color={theme.palette.primary.light}
       />
       <LinkButton
         siteURL={sitesURLs.idf}
-        iconURL='icons/idf.svg'
+        iconURL="icons/idf.svg"
         label='אתר צה"ל'
         color={theme.palette.info.light}
       />
       <LinkButton
         siteURL={sitesURLs.moodle}
-        iconURL='icons/moodle.svg'
-        label='מודל'
+        iconURL="icons/moodle.svg"
+        label="מודל"
         color={theme.palette.success.light}
       />
       <LinkButton
         siteURL={sitesURLs.drive}
-        iconURL='icons/googleDrive.svg'
-        label='דרייב קהד'
-        color={'#BFACC8'}
+        iconURL="icons/googleDrive.svg"
+        label="דרייב קהד"
+        color={"#BFACC8"}
       />
     </div>
   );
