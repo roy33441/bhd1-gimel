3a2351c15d61ff89db21bb0f9582bc3cfb9f27dc
Roy Canani <roy33441c@gmail.com>
2022-08-20 19:45:05 +0300

    finish

--- src/components/TopBar/TopBarStyles.tsx
+++ src/components/TopBar/TopBarStyles.tsx
@@ -1,26 +1,26 @@
-import { Theme } from '@material-ui/core/styles';
-import makeStyles from '@material-ui/core/styles/makeStyles';
+import { Theme } from "@material-ui/core/styles";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
 export const useStyles = makeStyles((theme: Theme) => ({
   topBar: {
     backgroundColor: theme.palette.secondary.light,
     paddingTop: theme.spacing(1),
     paddingRight: theme.spacing(1),
-    display: 'flex',
-    justifyContent: 'space-between'
+    display: "flex",
+    justifyContent: "space-between",
   },
   roleChip: {
-    width: '100%'
+    width: "100%",
   },
   drawerContainer: {
-    backgroundColor: '#D4D8EC'
+    backgroundColor: "#D4D8EC",
   },
   iconButton: {
-    backgroundColor: 'transparent',
-    border: 'transparent',
-    borderRadius: '50%',
+    backgroundColor: "transparent",
+    border: "transparent",
+    // borderRadius: '50%',
     width: theme.spacing(9.5),
     marginLeft: theme.spacing(0.8),
-    marginTop: theme.spacing(-0.5)
-  }
+    marginTop: theme.spacing(-0.5),
+  },
 }));
