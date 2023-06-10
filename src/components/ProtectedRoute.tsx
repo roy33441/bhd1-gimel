c1b6bbb0f866d4135b22e9d92d57f8a736481fac
Roy Canani <roy33441c@gmail.com>
2023-06-11 01:27:30 +0300

    update base path

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,9 +1,13 @@
-import { FC } from 'react';
-import { Redirect, Route, RouteProps } from 'react-router';
+import { FC } from "react";
+import { Redirect, Route, RouteProps } from "react-router";
 
-import auth from 'common/auth';
+import auth from "common/auth";
 
-const ProtectedRoute: FC<RouteProps> = routeProps =>
-  auth.isLoggedTzoer() ? <Route {...routeProps} /> : <Redirect to={'/login'} />;
+const ProtectedRoute: FC<RouteProps> = (routeProps) =>
+  auth.isLoggedTzoer() ? (
+    <Route {...routeProps} />
+  ) : (
+    <Redirect to={"/bhd1-gimel/login"} />
+  );
 
 export default ProtectedRoute;
--- src/components/WithAuthentication.tsx
+++ src/components/WithAuthentication.tsx
@@ -1,13 +1,15 @@
-import { FC } from 'react';
-import { Redirect } from 'react-router';
+import { FC } from "react";
+import { Redirect } from "react-router";
 
-import auth from 'common/auth';
+import auth from "common/auth";
 
 interface WithAuthenticationProps {
   children: JSX.Element;
 }
 
-const WithAuthentication: FC<WithAuthenticationProps> = ({ children }): JSX.Element =>
-  auth.isLoggedTzoer() ? children : <Redirect to={'/login'} />;
+const WithAuthentication: FC<WithAuthenticationProps> = ({
+  children,
+}): JSX.Element =>
+  auth.isLoggedTzoer() ? children : <Redirect to={"/bhd1-gimel/login"} />;
 
 export default WithAuthentication;
