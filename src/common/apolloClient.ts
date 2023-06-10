269c8da03431d491d2af070bf84499e96d3cd32a
Roy Canani <roy33441c@gmail.com>
2023-06-11 01:06:17 +0300

    update homepage

--- src/common/apolloClient.ts
+++ src/common/apolloClient.ts
@@ -1,30 +1,35 @@
-import { ApolloClient, InMemoryCache, HttpLink, split } from '@apollo/client';
-import { getMainDefinition } from '@apollo/client/utilities';
-import { WebSocketLink } from '@apollo/client/link/ws';
+import { ApolloClient, InMemoryCache, HttpLink, split } from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { WebSocketLink } from "@apollo/client/link/ws";
 
 const wsLink = new WebSocketLink({
-  uri: `wss://${process.env.REACT_APP_HASURA_URI}/v1/graphql`, // use wss for a secure endpoint
+  uri: `wss://wise-starfish-11.hasura.app/v1/graphql`, // use wss for a secure endpoint
   options: {
     reconnect: true,
     connectionParams: {
       headers: {
-        'x-hasura-admin-secret': process.env.REACT_APP_HASURA_ADMIN_SECRET
-      }
-    }
-  }
+        "x-hasura-admin-secret":
+          "EI0EnecW2i1B700UxvsivQgj0OfPAIA4y8fvQ54V9rlGlTkWJa1V6G54BqUw81uD",
+      },
+    },
+  },
 });
 
 const httpLink = new HttpLink({
-  uri: `https://${process.env.REACT_APP_HASURA_URI}/v1/graphql`,
+  uri: `https://wise-starfish-11.hasura.app/v1/graphql`,
   headers: {
-    'x-hasura-admin-secret': process.env.REACT_APP_HASURA_ADMIN_SECRET
-  }
+    "x-hasura-admin-secret":
+      "EI0EnecW2i1B700UxvsivQgj0OfPAIA4y8fvQ54V9rlGlTkWJa1V6G54BqUw81uD",
+  },
 });
 
 const splitLink = split(
   ({ query }) => {
     const definition = getMainDefinition(query);
-    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
   },
   wsLink,
   httpLink
@@ -33,6 +38,6 @@ const splitLink = split(
 export const apolloClient = new ApolloClient({
   link: splitLink,
   cache: new InMemoryCache({
-    addTypename: false
-  })
+    addTypename: false,
+  }),
 });
